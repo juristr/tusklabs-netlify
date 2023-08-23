1babd617c20c40112b4b748fabbd243e6093ce39
Juri <juri.strumpflohner@gmail.com>
2023-08-23 09:43:58 +0200

    add simple routing

--- apps/product-store/src/app/app.tsx
+++ apps/product-store/src/app/app.tsx
@@ -5,12 +5,30 @@ import NxWelcome from './nx-welcome';
 import { ProductsProductList } from '@tusklabs/products/product-list';
 import { OrdersOrderList } from '@tusklabs/orders/order-list';
 import { authenticationAuth } from '@tusklabs/authentication/auth';
+import { Route, Routes, Link } from 'react-router-dom';
 
 export function App() {
   return (
-    <div>
-      <NxWelcome title="product-store" />
-    </div>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+          <li>
+            <Link to="/orders">Orders</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<NxWelcome title="Product Store" />}></Route>
+        <Route path="/products" element={<ProductsProductList />}></Route>
+        <Route path="/orders" element={<OrdersOrderList />}></Route>
+      </Routes>
+    </>
   );
 }
 
